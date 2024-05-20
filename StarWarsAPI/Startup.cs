68fd5efbfd9d71387e528717ed428335a475bfaa
NathanMotoki <Nathan_Motoki@hotmail.com>
2024-05-20 09:57:32 -0300

    Preparando Ambiente

---
+++ StarWarsAPI/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+
+namespace StarWarsAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+    }
+}
